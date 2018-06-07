85197baf6f9bd7f0eb8a924871fd0833f4df175a
zhangshixiang <642410497@qq.com>
2018-06-07 18:30:32 +0800

    00

--- app/src/main/java/com/example/yikezhong123/MainActivity.java
+++ app/src/main/java/com/example/yikezhong123/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.yikezhong123;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -42,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private BottomBar bottomBar;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
